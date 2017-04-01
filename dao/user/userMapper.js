236362a5f62ecbd2aea7d66f70876523737f9ac4
David Tse <mask79513@sina.com>
2017-04-01 21:15:44 +0800

    add sql mapping dao for user

---
+++ dao/user/userMapper.js
@@ -0,0 +1,13 @@
+/**
+ * Created by David Xie on 2017/3/31.
+ * CURD Sql sentences
+ */
+var user = {
+  insert: 'insert into user(del_mark, username, password, role_id, from_where) values(0,?,?,?,?)',
+  delete: 'update user set del_mark = ? where id = ?',
+  update: 'update user set username = ?, password = ? where id = ?',
+  queryById: 'select * from user where id = ?',
+  queryAll: 'select * from user',
+  queryByName:'select * from user where username = ?'
+};
+module.exports = user ;
