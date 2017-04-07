7558083011321144cf97ece7e505f20f11d9a23c
David Tse <mask79513@sina.com>
2017-04-07 23:27:49 +0800

    revise userMapper

--- dao/userMapper.js
+++ dao/userMapper.js
@@ -4,9 +4,9 @@
  */
 export default  {
   insert: 'insert into user(del_mark, username, password, role_id, from_where) values(0,?,?,?,?)',
-  delete: 'update user set del_mark = ? where id = ?',
+  delete: 'update user set del_mark = 1 where id = ?',
   update: 'update user set password = ? where id = ?',
   queryById: 'select * from user where id = ?',
-  queryAll: 'select * from user',
+  queryAll: 'select * from user where del_mark = 0',
   queryByName:'select * from user where username = ?'
 };
