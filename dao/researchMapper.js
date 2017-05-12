5f0623d26e17c6a6e33ced233446f71c18ee99d0
David Tse <mask79513@sina.com>
2017-05-12 18:41:59 +0800

    modify service and mapper within research module

--- dao/researchMapper.js
+++ dao/researchMapper.js
@@ -6,25 +6,25 @@ export default {
   ' target, start_date, end_date, publisher, receiver) values(0,' +
   ' 0, ?, ?, ?, ?, ?, ?, ?, ?)',
   accept:'update research set received = 1,received_time = ? where id = ?',
-  refuse:'update research set received = -1 where id = ?',
+  refuse:'update research set received = -1, rejected_reason = ?, received_time = ? where id = ?',
   commit: 'update research set photo = ?, content = ?' +
   ' where receiver = ? and id = ?',
   delete: 'update research set del_mark = 1 where id = ?',
   queryForReceiver:'select * from research where received = 0 ' +
   'and del_mark = 0 and receiver = ?' +
-  'order by creation_time',
+  'order by creation_time desc',
   queryAcceptedForReceiver:'select * from research where received = 1 ' +
   'and del_mark = 0 and receiver = ?' +
-  'order by received_time',
+  'order by received_time desc',
   queryRefusedForReceiver:'select * from research where received = -1 ' +
   'and del_mark = 0 and receiver = ?' +
-  'order by creation_time',
+  'order by creation_time desc',
   queryForPublisher:'select * from research where ' +
-  'del_mark = 0 and publisher = ? order by creation_time desc',
+  'del_mark = 0 and received = 0 and publisher = ? order by creation_time desc',
   queryAcceptedForPublisher:'select * from research where received = 1 ' +
   'and del_mark = 0 and publisher = ?' +
-  'order by received_time',
+  'order by received_time desc',
   queryRefusedForPublisher:'select * from research where received = -1 ' +
   'and del_mark = 0 and publisher = ?' +
-  'order by creation_time',
+  'order by creation_time desc',
 }
--- service/resService.js
+++ service/resService.js
@@ -41,7 +41,7 @@ export default {
   },
   refuse(req, res, next){
     pool.getConnection(function (err, connection) {
-      connection.query(researchMapper.refuse, req.query.id, function (err, result) {
+      connection.query(researchMapper.refuse, [req.query.rejectedReason, new Date().toLocaleString(), req.query.id], function (err, result) {
         if (result){
           result = {
             code:200,
